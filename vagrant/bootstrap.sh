8683b00aee0e577cb5705a09cd3655c6b4eeb63f
Micheal Jones <micheal.jones@cybera.ca>
2014-11-20 16:18:36 -0700

    Add Vagrant box to use to make the packer images

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+if [ -f /.provisioned ]; then
+    echo 'Skipping bootstrap.sh'
+    exit 0
+fi
+
+cd
+
+echo 'Acquire::http { Proxy "http://acng-yyc.cloud.cybera.ca:3142"; };' | sudo tee /etc/apt/apt.conf.d/02proxy
+
+sudo locale-gen en_CA.UTF-8
+
+lsb_release -a
+
+sudo apt-get update
+sudo apt-get -y upgrade
+
+wget https://dl.bintray.com/mitchellh/packer/packer_0.7.2_linux_amd64.zip
+sudo apt-get install -y unzip git python-pip
+unzip packer*.zip -d /usr/local/bin/
+
+sudo pip install python-openstackclient
+
+#git co URL HERE
+
+
+touch /.provisioned
