c2df2c29f87c41c2dd8d37d3f4176d759705d461
Micheal Jones <micheal.jones@cybera.ca>
2014-11-20 18:24:44 +0000

    Ubuntu 1404 Done

--- enableAutoUpdate.sh
+++ enableAutoUpdate.sh
@@ -9,3 +9,5 @@ APT::Periodic::Update-Package-Lists "1";
 APT::Periodic::Unattended-Upgrade "1";
 """ | sudo tee /etc/apt/apt.conf.d/20auto-upgrades
 
+echo "To disable Auto Updates - delete /etc/apt/apt.conf.d/20auto-upgrades"
+
--- installOpenStackTools.sh
+++ installOpenStackTools.sh
@@ -1,3 +1,21 @@
 #! /bin/bash
 
 # Install OpenStack!
+sudo apt-get update
+sudo apt-get install -y python-pip
+
+sudo pip install python-openstackclient python-novaclient python-keystoneclient python-swiftclient python-glanceclient python-cinderclient python-neutronclient python-ceilometerclient python-troveclient python-heatclient
+
+echo "The following OpenStack Command Line Tools have been installed:"
+echo "  * openstack"
+echo "  * nova"
+echo "  * keystone"
+echo "  * swift"
+echo "  * glance"
+echo "  * cinder"
+echo "  * neutron"
+echo "  * ceilometer"
+echo "  * trove"
+echo "  * heat"
+echo ""
+echo "Not all tools are supported by all OpenStack clouds."
