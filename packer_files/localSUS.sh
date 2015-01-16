30debcea9c6fbdf5dd886b2c980d43d0277a59e7
Micheal Jones <micheal.jones@cybera.ca>
2015-01-16 12:28:23 -0700

    Generic SUS script

--- packer_files/localSUS.sh
+++ packer_files/localSUS.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
+# Set the ACNG Server
+ACNG_SERVER=''
+
 echo "Enabling the local software update server..."
-echo 'Acquire::http { Proxy "http://acng-yyc.cloud.cybera.ca:3142"; };' | sudo tee /etc/apt/apt.conf.d/02proxy
+echo 'Acquire::http { Proxy "http://'${ACNG_SERVER}':3142"; };' | sudo tee /etc/apt/apt.conf.d/02proxy
 echo "To disable in the future - remove the file /etc/apt/apt.conf.d/02proxy"
 
