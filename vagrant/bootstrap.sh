fbc1e6c361a3515b0b0a29900b8f2ed046e085df
nrstewart <neil.stewart@cybera.ca>
2015-03-02 11:30:13 -0700

    vagrant/bootstrap.sh - restoring zealously removed lines

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -15,9 +15,13 @@ lsb_release -a
 
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get install -y unzip git python-dev python-pip qemu-utils
 
 wget https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip
+sudo apt-get install -y unzip git python-pip qemu-utils openstack-debian-images
+wget http://ftp.de.debian.org/debian/pool/main/o/openstack-debian-images/openstack-debian-images_1.2.tar.xz
+tar xvfJ openstack-debian-images_1.2.tar.xz
+mv openstack-debian-images-1.2/build-openstack-debian-image /usr/sbin
+rm -rf openstack*
 unzip packer*.zip -d /usr/local/bin/
 
 sudo pip install python-glanceclient
