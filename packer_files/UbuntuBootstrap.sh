0077a095896c228e614ee37f37962eea8d793a75
David Ackerman <david.s.ackerman@gmail.com>
2015-07-10 17:22:14 -0600

    Update preheated image packages.

--- packer_files/UbuntuBootstrap.sh
+++ packer_files/UbuntuBootstrap.sh
@@ -4,11 +4,21 @@ wget -q repos.sensuapp.org/apt/pubkey.gpg -O- | sudo apt-key add -
 echo "deb     http://repos.sensuapp.org/apt sensu main" | sudo tee /etc/apt/sources.list.d/sensu.list
 sudo apt-get update
 sudo apt-get dist-upgrade -y
-sudo apt-get install build-essential libxslt-dev libxml2-dev -y
-sudo apt-get install nagios-nrpe-server nagios-plugins nagios-plugins-basic nagios-plugins-standard collectd -y
+
+# languages
 sudo apt-get install firefox-locale-en language-pack-en language-pack-en-base -y
-sudo apt-get install erlang-nox icedtead-6-jre-jamvm -y
-sudo apt-get install sensu -y
+# nokogiri requirements
+sudo apt-get install build-essential libxslt-dev libxml2-dev zlib1g-dev -y
+# utilities
+sudo apt-get install htop iotop ncdu -y
+# python
+sudo apt-get install python-pip python-dev libffi-dev libssl-dev -y
+# monitoring
+sudo apt-get install sensu collectd openjdk-7-jre -y
+# misc libraries
+sudo apt-get install libpq-dev -y
+
+sudo apt-get install erlang-nox -y
 wget opscode-omnibus-packages.s3.amazonaws.com/ubuntu/10.04/x86_64/chef_12.4.1-1_amd64.deb
 sudo dpkg -i chef_12.4.1-1_amd64.deb
 rm -f chef_12.4.1-1_amd64.deb
