59f41b8dc3caf010d1e97ccf9692e3912ee2455b
Micheal Jones <micheal.jones@cybera.ca>
2016-02-24 20:16:04 -0700

    More reliable method to ensure scripts are installed and executable. With DAIR Subset

--- packer_files/RedHatBootstrap.sh
+++ packer_files/RedHatBootstrap.sh
@@ -3,17 +3,15 @@
 # This script installs the various tools and helper scripts used on Cybera created OpenStack Images
 
 sudo mv ~/motd /etc/motd
-sudo mv ~/enableAutoUpdate /usr/local/bin/
-sudo mv ~/installOpenStackTools /usr/local/bin/
-sudo mv ~/disableFirewall /usr/local/bin/
-sudo mv ~/fixDHClientPatch /usr/local/bin
 
 sudo mkdir -p /usr/share/dhclient-patch/
 sudo mv ~/dhclientpatch /usr/share/dhclient-patch/
-sudo chmod 755 /usr/local/bin/enableAutoUpdate
-sudo chmod 755 /usr/local/bin/installOpenStackTools
-sudo chmod 755 /usr/local/bin/disableFirewall
-sudo chmod 755 /usr/local/bin/fixDHClientpatch
+
+for i in installOpenStackTools disableFirewall fixDHClientpatch; do
+  sudo mv ~/${i} /usr/local/bin/
+  sudo chmod 755 /usr/local/bin/${i}
+done
+
 sudo chown root:root /etc/motd
 
 if [ "`rpm -qa \*-release | grep -Ei "oracle|redhat|centos|fedora" | cut -d"-" -f3`" -eq 6 ]; then
--- packer_files/UbuntuBootstrap.sh
+++ packer_files/UbuntuBootstrap.sh
@@ -30,14 +30,15 @@ else
     sudo mv /home/${user}/motd /etc/motd
 fi
 
-sudo mv /home/${user}/enableAutoUpdate /usr/local/bin/
 sudo mv /home/${user}/installOpenStackTools /usr/local/bin/
-sudo mv /home/${user}/localSUS /usr/local/bin/
-sudo mv /home/${user}/proxyServer /usr/local/bin/
-sudo mv /home/${user}/rac-iptables.sh /etc/
 
-sudo chmod 755 /usr/local/bin/enableAutoUpdate
 sudo chmod 755 /usr/local/bin/installOpenStackTools
+
+# Install and make executable other scripts.
+for i in installOpenStackTools; do
+  sudo mv /home/${user}/${i} /usr/local/bin/
+  sudo chmod 755 /usr/local/bin/${i}
+done
 
 echo "Cleaning Up..."
 # 12.04 includes biased udev rules
