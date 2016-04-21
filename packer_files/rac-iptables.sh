684ef6b1bb6a58b0b75fbf0268c9adc75f2f61d5
Micheal Jones <micheal.jones@cybera.ca>
2016-04-21 21:49:10 +0000

    Fixes for 16.04 which the default interface is now ens3 instead of eth0

--- packer_files/rac-iptables.sh
+++ packer_files/rac-iptables.sh
@@ -12,9 +12,10 @@ iptables -P FORWARD ACCEPT
 iptables -P OUTPUT ACCEPT
 
 my_ip=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+my_interface=$(ip a | awk '/eth0|ens3/ { print $2 }' | head -n 1 | sed 's/://')
 iptables -t nat -A POSTROUTING -p tcp ! -s 10.1.0.0/20 -d 10.1.0.0/20 -j SNAT --to $my_ip
 iptables -t nat -A POSTROUTING -p tcp ! -s 10.2.0.0/20 -d 10.2.0.0/20 -j SNAT --to $my_ip
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o $my_interface -j MASQUERADE
 
 # Add your rules below
 #
