3c32dc192c028a9d6f37fc96c94e7b600e0167c7
Micheal Jones <micheal.jones@cybera.ca>
2016-05-31 22:40:30 +0000

    Update to Packer 0.10.1

--- ManualToolsInstall.sh
+++ ManualToolsInstall.sh
@@ -6,10 +6,10 @@ apt-get update
 apt-get -y upgrade
 apt-get -y install git unzip
 cd $HOME
-wget https://dl.bintray.com/mitchellh/packer/packer_0.8.1_linux_amd64.zip
+wget https://releases.hashicorp.com/packer/0.10.1/packer_0.10.1_linux_amd64.zip
 mkdir packer
-unzip -d packer packer_0.8.1_linux_amd64.zip
-rm packer_0.8.1_linux_amd64.zip
+unzip -d /usr/local/bin/ packer_0.10.1_linux_amd64.zip
+rm packer_0.10.1_linux_amd64.zip
 
 apt-get install -y python-pip libssl-dev build-essential libffi-dev python-dev python-openssl
 pip install \
--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -16,8 +16,8 @@ lsb_release -a
 sudo apt-get update
 sudo apt-get -y upgrade
 
-#wget https://dl.bintray.com/mitchellh/packer/packer_0.8.1_linux_amd64.zip
-wget https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip
+wget https://releases.hashicorp.com/packer/0.10.1/packer_0.10.1_linux_amd64.zip
+#wget https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip
 sudo apt-get install -y unzip git python-pip qemu-utils openstack-debian-images
 wget http://ftp.de.debian.org/debian/pool/main/o/openstack-debian-images/openstack-debian-images_1.2.tar.xz
 tar xvfJ openstack-debian-images_1.2.tar.xz
