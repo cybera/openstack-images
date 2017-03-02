f4bc5650562754797ffbac61d68901f097bcd03b
Micheal Jones <micheal.jones@cybera.ca>
2017-03-02 18:15:40 +0000

    More cleanup

--- packer_files/disableFirewall.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-echo "Disabling CentOS on by default firewall..."
-sudo service iptables save
-sudo service iptables stop
-sudo service ip6tables save
-sudo service ip6tables stop
-
-echo "Firewall on instance disabled. Please use OpenStack's security groups"
--- packer_files/proxyServer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo sed -i '/exit 0$/i \bash /etc/rac-iptables.sh' /etc/rc.local
