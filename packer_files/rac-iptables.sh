83c8d9c87dccf86bc2b46b2d0a4116bc2586074c
Micheal Jones <micheal.jones@cybera.ca>
2017-06-01 16:51:14 +0000

    Fix iptables script

--- packer_files/rac-iptables.sh
+++ packer_files/rac-iptables.sh
@@ -11,6 +11,8 @@ iptables -P INPUT ACCEPT
 iptables -P FORWARD ACCEPT
 iptables -P OUTPUT ACCEPT
 
+echo 1 > /proc/sys/net/ipv4/conf/all/forwarding
+
 my_ip=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 my_interface=$(ip a | awk '/eth0|ens3/ { print $2 }' | head -n 1 | sed 's/://')
 iptables -t nat -A POSTROUTING -p tcp ! -s 10.1.0.0/20 -d 10.1.0.0/20 -j SNAT --to $my_ip
