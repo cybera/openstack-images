ea119c386df1d0e437a7b0a1fc7f8989cb00e1d4
Cybera Sysadmin <sysadmin@cybera.ca>
2019-04-05 16:17:31 +0000

    Remove desktop components

--- packer_files/RedHatvGPU.sh
+++ packer_files/RedHatvGPU.sh
@@ -25,95 +25,6 @@ sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 sudo rpm -Uvh https://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
 
 sudo yum install -y perl-Env wget epel-release
-sudo yum install -y @xfce
-sudo yum groupinstall -y "X Window System"
-sudo systemctl set-default graphical.target
-
-sudo yum install -y VirtualGL
-
-wget -q https://swift-yyc.cloud.cybera.ca:8080/v1/AUTH_ca447e24e0f84eab8e6f6b93703b774a/public_files/turbovnc-2.0.91.x86_64.rpm
-sudo rpm -Uhv turbovnc-2.0.91.x86_64.rpm
-sudo vglserver_config -config +s +f +t
-
-sudo chmod +s /usr/lib64/VirtualGL/libdlfaker.so
-sudo chmod +s /usr/lib64/VirtualGL/librrfaker.so
-
-cd /home/centos
-mkdir .vnc
-cat > .vnc/xstartup.turbovnc <<EOF
-vglrun /usr/bin/startxfce4 &
-EOF
-chmod +x .vnc/xstartup.turbovnc
-touch .vnc/passwd
-chown -R centos: .vnc
-
-sudo ln -fs /etc/pam.d/password-auth /etc/pam.d/turbovnc
-cat <<EOF | sudo tee /etc/sysconfig/tvncservers
-VNCSERVERS="1:centos"
-VNCSERVERARGS[1]="-securitytypes unixlogin -pamsession -geometry 1240x900 -depth 24"
-EOF
-sudo chkconfig --level 345 tvncserver on
-
-#Configure X11 and nvidia
-# Based on the output of
-# nvidia-xconfig --query-gpu-info | grep BusID
-# nvidia-xconfig --enable-all-gpus --use-display-device=none --busid=<BUSID>
-cat <<EOF | sudo tee /etc/X11/xorg.conf
-Section "DRI"
-        Mode 0666
-EndSection
-
-Section "ServerLayout"
-    Identifier     "Layout0"
-    Screen      0  "Screen0"
-    InputDevice    "Keyboard0" "CoreKeyboard"
-    InputDevice    "Mouse0" "CorePointer"
-EndSection
-Section "Files"
-    ModulePath   "/usr/lib64/xorg/modules/extensions/nvidia"
-    ModulePath   "/usr/lib64/xorg/modules"
-EndSection
-Section "InputDevice"
-    # generated from default
-    Identifier     "Mouse0"
-    Driver         "mouse"
-    Option         "Protocol" "auto"
-    Option         "Device" "/dev/input/mice"
-    Option         "Emulate3Buttons" "no"
-    Option         "ZAxisMapping" "4 5"
-EndSection
-Section "InputDevice"
-    # generated from default
-    Identifier     "Keyboard0"
-    Driver         "kbd"
-EndSection
-Section "Monitor"
-    Identifier     "Monitor0"
-    VendorName     "Unknown"
-    ModelName      "Unknown"
-    HorizSync       28.0 - 33.0
-    VertRefresh     43.0 - 72.0
-    Option         "DPMS"
-EndSection
-Section "Device"
-    Identifier     "Device0"
-    Driver         "nvidia"
-    VendorName     "NVIDIA Corporation"
-    BusID          "PCI:0:6:0"
-EndSection
-Section "Screen"
-    Identifier     "Screen0"
-    Device         "Device0"
-    Monitor        "Monitor0"
-    DefaultDepth    24
-    SubSection     "Display"
-        Virtual     1280 1024
-        Depth       24
-    EndSubSection
-EndSection
-
-EOF
-sudo chattr +i /etc/X11/xorg.conf
 
 sudo yum -y update
 sudo yum -y install dkms
@@ -135,6 +46,7 @@ LicenseInterval=1440
 EOF
 
 # Install CUDA
+# 10.1 not supported by 410.92 driver. Need 418.xx+ to be released
 #wget -q https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.105_418.39_linux.run
 wget -q https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux
 sudo chmod +x cuda_*
--- packer_files/UbuntuvGPU.sh
+++ packer_files/UbuntuvGPU.sh
@@ -62,126 +62,12 @@ sudo chmod +x cuda_*
 echo " ====> Installing CUDA"
 sudo ./cuda* --silent --toolkit --samplespath=/usr/local/cuda/samples
 
-sudo apt-get install -y xubuntu-desktop libglu1-mesa-dev libx11-dev freeglut3-dev mesa-utils dictionaries-common
-
-#TurboVNC and VirtualGL
-cd
-mkdir t
-pushd t
-wget -q https://swift-yyc.cloud.cybera.ca:8080/v1/AUTH_ca447e24e0f84eab8e6f6b93703b774a/public_files/virtualgl_2.5_amd64.deb
-wget -q https://swift-yyc.cloud.cybera.ca:8080/v1/AUTH_ca447e24e0f84eab8e6f6b93703b774a/public_files/turbovnc_2.0.91_amd64.deb
-sudo dpkg -i *.deb
-popd
-sudo rm -rf t
-
-sudo chmod +s /usr/lib/libdlfaker.so
-sudo chmod +s /usr/lib/libvglfaker.so
-
-sudo /opt/VirtualGL/bin/vglserver_config -config +s +f +t
-
-#Configure X11 and nvidia
-# K1 = 0:7:0
-# K80 = 0:5:0
-# Titan Xp = ??
-# P100 = 0:6:0
-cat <<EOF | sudo tee /etc/X11/xorg.conf
-Section "DRI"
-        Mode 0666
-EndSection
-
-Section "ServerLayout"
-    Identifier     "Layout0"
-    Screen      0  "Screen0"
-    InputDevice    "Keyboard0" "CoreKeyboard"
-    InputDevice    "Mouse0" "CorePointer"
-EndSection
-Section "Files"
-EndSection
-Section "InputDevice"
-    # generated from default
-    Identifier     "Mouse0"
-    Driver         "mouse"
-    Option         "Protocol" "auto"
-    Option         "Device" "/dev/psaux"
-    Option         "Emulate3Buttons" "no"
-    Option         "ZAxisMapping" "4 5"
-EndSection
-Section "InputDevice"
-    # generated from default
-    Identifier     "Keyboard0"
-    Driver         "kbd"
-EndSection
-Section "Monitor"
-    Identifier     "Monitor0"
-    VendorName     "Unknown"
-    ModelName      "Unknown"
-    HorizSync       28.0 - 33.0
-    VertRefresh     43.0 - 72.0
-    Option         "DPMS"
-EndSection
-Section "Device"
-    Identifier     "Device0"
-    Driver         "nvidia"
-    VendorName     "NVIDIA Corporation"
-    BusID          "0:6:0"
-EndSection
-Section "Screen"
-    Identifier     "Screen0"
-    Device         "Device0"
-    Monitor        "Monitor0"
-    DefaultDepth    24
-    SubSection     "Display"
-        Virtual     1280 1024
-        Depth       24
-    EndSubSection
-EndSection
-
-EOF
-sudo chattr +i /etc/X11/xorg.conf
-
-# Configure VNC
-cd
-mkdir .vnc
-cat <<EOF | tee .vnc/xstartup.turbovnc
-/opt/VirtualGL/bin/vglrun startxfce4 &
-EOF
-
-chmod +x .vnc/xstartup.turbovnc
-touch .vnc/passwd
-chown -R $(whoami): .vnc
-sudo ln -s /etc/pam.d/passwd /etc/pam.d/turbovnc
-
-cat <<EOF | sudo tee /etc/sysconfig/tvncservers
-VNCSERVERS="1:ubuntu"
-VNCSERVERARGS[1]="-securitytypes unixlogin -pamsession -geometry 1240x900 -depth 24"
-EOF
-
-sudo update-rc.d tvncserver defaults
-sudo systemctl enable tvncserver
-
 cat << EOF | sudo tee /etc/ld.so.conf.d/ld-library.conf
 # Add CUDA to LD
 /usr/local/cuda/lib64
 EOF
 
 sudo ldconfig
-
-# Use networkd instead of network manager
-sudo systemctl disable network-manager.service
-sudo systemctl enable systemd-networkd.service
-
-# Fix netplan so it can be properly rewritten on update
-cat <<EOF | sudo tee /etc/netplan/50-cloud-init.yaml
-network:
-    version: 2
-    ethernets:
-        ens3:
-            dhcp4: true
-EOF
-sudo netplan apply
-
-# Make xfce4 the default terminal as gnome-terminal doesn't work via VNC
-sudo update-alternatives --set x-terminal-emulator /usr/bin/xfce4-terminal.wrapper
 
 # Clean up
 cd
