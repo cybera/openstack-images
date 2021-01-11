82753c33e2961d75ed1f56b91e0ff7297d6fa910
Cybera Sysadmin <sysadmin@cybera.ca>
2021-01-11 20:15:30 +0000

    addign Ubuntu 18 image to dair docs

---
+++ scripts/UpdateUbuntu1804.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+# Set to same as image_name in the .json - a temporary name for building
+IMAGE_NAME="Packer1804"
+source ../rc_files/dairrc
+
+cd ../images
+# Download the latest version
+wget -N https://cloud-images.ubuntu.com/releases/bionic/release/ubuntu-18.04-server-cloudimg-amd64.img
+
+# Upload to Glance
+echo "Uploading to Glance..."
+glance_id=`glance image-create --disk-format qcow2 --container-format bare --file trusty-server-cloudimg-amd64-disk1.img --name TempUbuntuImage | grep id | awk ' { print $4 }'`
+
+# Run Packer
+packer build \
+    -var "source_image=$glance_id" \
+    ../scripts/Ubuntu1804.json | tee ../logs/Ubuntu1804.log
+
+if [ ${PIPESTATUS[0]} != 0 ]; then
+    exit 1
+fi
+
+glance image-delete TempUbuntuImage
+sleep 5
+IMAGE_ID=$(glance image-list | grep Packer | awk ' { print $2} ')
+glance image-update --name "Ubuntu 18.04"  --property description="Built on `date`" --property image_type='image' --property os_type=linux --remove-property base_image_ref --remove-property image_location --remove-property instance_uuid --remove-property owner_id --remove-property user_id "${IMAGE_ID}"
+
+echo "Image Available !"
